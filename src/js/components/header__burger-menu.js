2080da5e3bf1c54b56493a55d327e03b8ae25b64
hhdesus <hhdesus@bk.ru>
2024-02-22 16:46:50 +0300

    demo

--- src/js/components/header__burger-menu.js
+++ src/js/components/header__burger-menu.js
@@ -1,11 +1,9 @@
 // import { Dropdown } from "./plugins/dropdown"
 const $burger = document.querySelector('.burger')
-const $nav = document.querySelector('.nav')
-const $menu = document.querySelector('.menu')
+const $navMenu = document.querySelector('.header__menu')
 const burgerToggler = (e) => {
   $burger.classList.toggle('is-active')
-  $nav.classList.toggle('is-active')
-  $menu.classList.toggle('is-active')
+  $navMenu.classList.toggle('is-active')
 }
 
 $burger.addEventListener('click', burgerToggler)
